86ec9dfca99880c0fdb9e475864743557ebee28e
unknown <aleemsajjad1@gamil.com>
2020-06-08 04:52:45 -0700

    Updates

---
+++ precache-manifest.309f2d95c65c69f69e895a540e41fb18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdf55b85c563fa7f35f4ddd48b21298",
+    "url": "/pickle-deployment/index.html"
+  },
+  {
+    "revision": "7362505eccd53bb586ed",
+    "url": "/pickle-deployment/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "99d21af130fa8239884e",
+    "url": "/pickle-deployment/static/css/main.ea273115.chunk.css"
+  },
+  {
+    "revision": "7362505eccd53bb586ed",
+    "url": "/pickle-deployment/static/js/2.e95f6d40.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/pickle-deployment/static/js/2.e95f6d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d21af130fa8239884e",
+    "url": "/pickle-deployment/static/js/main.87da8af3.chunk.js"
+  },
+  {
+    "revision": "b4ba9f1b3d50e4556f13",
+    "url": "/pickle-deployment/static/js/runtime-main.d6818a51.js"
+  }
+]);
